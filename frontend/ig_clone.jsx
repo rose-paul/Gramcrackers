4e566842aeb86adbe11480cc4c7e05a7f3631804
Paul <prose0021@gmail.com>
2019-11-03 17:15:49 -0800

    made ajax utils for signup, login, logout

--- frontend/ig_clone.jsx
+++ frontend/ig_clone.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from './components/Root';
+import { login, logout } from "./util/session_api_util";
+
+
+window.login = login;
+window.logout = logout;
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
