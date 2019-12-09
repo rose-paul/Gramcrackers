7a6f4ea634707e4009c07b52e37ba4315d78abf9
Paul <prose0021@gmail.com>
2019-12-09 10:43:31 -0800

    add table for followers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_200004) do
+ActiveRecord::Schema.define(version: 2019_12_09_184251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2019_11_13_200004) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "followers", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "follower_id"], name: "index_followers_on_user_id_and_follower_id", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
