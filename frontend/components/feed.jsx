93e0bd59e5bf607cb9a7653351f17c4c52be1448
Paul <prose0021@gmail.com>
2019-12-11 15:46:07 -0800

    create Feed component w/o FeedIndexItem

--- frontend/components/feed.jsx
+++ frontend/components/feed.jsx
@@ -1,21 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import GreetingContainer from './greeting_container';
 const Feed = props => {
 
-    const [posts] = useState(props.posts)
+    const [posts, setPosts] = useState(props.posts)
+    const [currentUser] = useState(props.currentUser)
 
     useEffect( () => {
-        props.fetchFollows('following').then( res => 
-            props.fetchFeedPosts(Object.keys(res.follows))
-        )
-    })
-
-    console.log(posts)
+        props.fetchFollows('following')
+        .then( res => props.fetchFeedPosts(Object.keys(res.follows)))
+        .then( res => setPosts(res.posts))
+    }, [currentUser])
 
     return (
         <div className="feed-main">
-            <h2>feed coming soon</h2>
+            <h2>feed coming soon, {currentUser.username}</h2>
+            <ul>
+            {
+                Object.values(posts).map( post => {
+                    return <li key={post.id}>{post.caption}</li>
+                })
+            }
+            </ul>
             <img src="/russia.png" alt=""/>
         </div>
     )
