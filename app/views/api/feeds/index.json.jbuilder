679f79c632b8e682b86d09be7909a47641b848c2
Paul <prose0021@gmail.com>
2019-12-12 08:49:59 -0800

    add pic/username to feed items

--- app/views/api/feeds/index.json.jbuilder
+++ app/views/api/feeds/index.json.jbuilder
@@ -1,6 +1,10 @@
 @posts.each do |post| 
     json.set! post.id do 
         json.extract! post, :id, :user_id, :caption
+        json.username post.user.username
+        if post.user.photo.attached?
+            json.userPhoto url_for(post.user.photo) 
+        end
         json.photoUrl url_for(post.photo)
     end
 end
--- frontend/components/feed/feed_index_item.jsx
+++ frontend/components/feed/feed_index_item.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import Comment from '../comments/comment';
+
 
 const FeedIndexItem = props => {
+    let userPhoto = props.post.userPhoto ? <img className="profile-pic-small" src={props.post.userPhoto} /> : <img className="profile-pic-small" src="/user.png" />
     return (
-        <div>
-            <img src={props.post.photoUrl} alt=""/>
+        <div className="feed-item-whole">
+            <span>
+                {userPhoto} <b>{props.post.username}</b>
+            </span>
+            <img className="post" src={props.post.photoUrl} alt=""/>
             <p>{props.post.caption}</p>
         </div>
     )
