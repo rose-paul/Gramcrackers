305242cc77f21eee5f62033b05b1c3923b7767b1
Paul <prose0021@gmail.com>
2019-12-12 10:02:44 -0800

    add spinner for feed

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import FeedIndexItemContainer from './feed_index_item_container';
+import Loader from 'react-loader-spinner'
 
 
 const Feed = props => {
 
     const [posts, setPosts] = useState(props.posts)
     const [currentUser] = useState(props.currentUser)
+    const [loaded, setLoaded] = useState(false)
 
     useEffect( () => {
         props.fetchFollows('following')
         .then( res => props.fetchFeedPosts(Object.keys(res.follows)))
         .then( res => setPosts(res.posts))
+        .then( () => setLoaded(true))
     }, [currentUser])
 
-    return (
+    return loaded ? (
         <div className="feed-main">
             <header>
                 <h2>Here you are {currentUser.username}! </h2>
@@ -28,7 +31,7 @@ const Feed = props => {
             </ul>
             
         </div>
-    )
+    ) : <Loader type="Grid" color="rgb(98, 150, 209)" className="loading" />
 }
 export default Feed;
 
