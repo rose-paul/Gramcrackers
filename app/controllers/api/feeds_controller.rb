c490a60ec101f7231a1be86d7e698086f90d02fe
Paul <prose0021@gmail.com>
2020-01-03 14:55:54 -0800

    add basic infinite scroll

--- app/controllers/api/feeds_controller.rb
+++ app/controllers/api/feeds_controller.rb
@@ -4,10 +4,10 @@ class Api::FeedsController < ApplicationController
         user_ids.concat(params[:user_ids]) if params[:user_ids]
         user_ids.push(current_user.id)
         if !params[:margin]
-            @posts = Post.where(user_id: user_ids).limit(10)
+            @posts = Post.where(user_id: user_ids).limit(5)
             render :index
         else
-            @posts = Post.where(user_id: user_ids).offset(params[:margin]).limit(10)
+            @posts = Post.where(user_id: user_ids).offset(params[:margin]).limit(5)
         end
     end
 
--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import FeedIndexItemContainer from './feed_index_item_container';
 import Loader from 'react-loader-spinner'
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 
 const Feed = props => {
@@ -8,16 +10,28 @@ const Feed = props => {
     const [posts, setPosts] = useState(props.posts)
     const [currentUser] = useState(props.currentUser)
     const [loaded, setLoaded] = useState(false)
+    const [offset, setOffset] = useState(0)
     
+    useEffect(() => {
+      props
+        .fetchFollows("following")
+        .then(res => props.fetchFeedPosts(Object.keys(res.follows), offset))
+        .then(res => setPosts(res.posts))
+        .then(() => setOffset(offset + 5))
+        .then(() => setLoaded(true));
+    }, []);
 
-    useEffect( () => {
-        props.fetchFollows('following')
-        .then( res => props.fetchFeedPosts(Object.keys(res.follows), 5))
-        .then( res => setPosts(res.posts))
-        .then( () => setLoaded(true))
-    }, [])
+    const fetchMorePosts = () => {
+        props.fetchFollows("following").then( res =>
+            props.fetchFeedPosts(Object.keys(res.follows), offset)
+        )
+        .then(res => setPosts({...posts, ...res.posts}))
+        .then( () => setOffset(offset + 5))
+    }
 
     const reversedPosts = Object.values(posts).reverse()
+
+    console.log(posts)
 
     return loaded ? (
         <div className="feed-main">
@@ -25,11 +39,26 @@ const Feed = props => {
                 <h2>Here you are, {currentUser.username}! </h2>
             </header>
             <ul className="feed-list">
+                <InfiniteScroll
+            id="feed-infinite"
+            dataLength={Object.keys(posts).length}
+            next={fetchMorePosts}
+            hasMore={true}
+            loader={
+            <div className="feed-fetching-more">
+              <img src='001-camera.png' />
+            </div>}
+            endMessage={
+              <div>
+                No more posts.
+            </div>
+            }>
             {
                 reversedPosts.map( post => {
                     return (<li><FeedIndexItemContainer key={post.id} post={post} /></li>)
                 })
             }
+            </InfiniteScroll>
             </ul>
             
         </div>
