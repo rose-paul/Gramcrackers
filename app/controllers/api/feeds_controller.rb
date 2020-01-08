7b1474403489b48c22a26f2ae76df1fce933d722
Paul <prose0021@gmail.com>
2020-01-07 22:48:20 -0800

    refactor search bar

--- app/controllers/api/feeds_controller.rb
+++ app/controllers/api/feeds_controller.rb
@@ -4,10 +4,10 @@ class Api::FeedsController < ApplicationController
         user_ids.concat(params[:user_ids]) if params[:user_ids]
         user_ids.push(current_user.id)
         if !params[:margin]
-            @posts = Post.where(user_id: user_ids).limit(5)
+            @posts = Post.where(user_id: user_ids).limit(5).reverse
             render :index
         else
-            @posts = Post.where(user_id: user_ids).offset(params[:margin]).limit(5)
+            @posts = Post.where(user_id: user_ids).offset(params[:margin]).limit(5).reverse
         end
     end
 
